5f27274f3d95d9a9f95ace80a6d7e5f6e3356e78
matheusfgomes <matheusfgomes935@gmail.com>
2019-11-22 09:17:28 -0300

    update 1.0

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Chato' },
+  { id: 12, name: 'Narcobolizante' },
+  { id: 13, name: 'Bombaste na prova??' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr Professor' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
